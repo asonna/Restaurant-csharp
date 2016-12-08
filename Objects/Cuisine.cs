7a860b030ce8ce2a834f041d3946338771fbb717
Annie Sonna and Jesse McKay <annie+jesse@>
2016-12-07 16:42:52 -0800

    create cuisine class

---
+++ Objects/Cuisine.cs
@@ -0,0 +1,18 @@
+// using System.Collections.Generic;
+// using System.Data.SqlClient;
+// using System;
+//
+// namespace BestRestaurant
+// {
+//   public class Cuisine
+//   {
+//     private int _id; //Cuisine ID
+//     private string _type; //Restaurant Name
+//
+//     public Restaurant(int Id = 0, string type)
+//     {
+//       _id = Id;
+//       _type = type;
+//     }
+//   }
+// }
